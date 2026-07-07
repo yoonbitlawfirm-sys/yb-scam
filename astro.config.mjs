b2ede4aabd36eea24bf32093d014960907de7966
김지민 <yoonbitlawfirm@gmail.com>
2026-07-07 15:01:14 +0900

    add compatible vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://www.yb-scam.com",
-  output: "server"
+  output: "server",
+  adapter: vercel(),
 });
